019bdb5be1a14e95ab484452080fffcd875d2234
Christopher Kennedy <ckennedy@ellation.com>
2019-11-25 21:25:04 +0000

    update ffmpeg patch
    update ffmpeg patch with frame hamming limited to hash max sig
    value.
    
    vp9 cmd added vbv back

--- scripts/gen_cmd.sh
+++ scripts/gen_cmd.sh
@@ -33,7 +33,7 @@ while [ $b -le 8000 ]; do
 
     #printf "\n%05dSVTVP9|ffmpeg|vbr||mp4|-pix_fmt|yuv420p|-f|mp4|-vcodec|libsvt_vp9|-preset|5|-tune|2|-b:v|%dk|-maxrate:v|%0.0fk|-bufsize:v|%0.0fk|-minrate:v|%dk|-rc|vbr|-keyint_min|48|-g|48|-hide_banner|-nostats;\\" $b $b $(echo "$b * 1.5" | bc -l) $(echo "$b * 3" | bc -l) $b
 
-    printf "\n%05dVPXVP9|ffmpeg|twopass|S|mp4|-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-vcodec|libvpx-vp9|-speed|0|-b:v|%dk|-g|48|-hide_banner|-nostats|-row-mt|1|-tune-content|0|-tile-columns|1|-aq-mode|0|-overshoot-pct|0|-undershoot-pct|0|-auto-alt-ref|1;\\" $b $b
+    printf "\n%05dVPXVP9|ffmpeg|twopass|S|mp4|-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-vcodec|libvpx-vp9|-speed|0|-b:v|%dk|-maxrate:v|%0.0fk|-bufsize:v|%0.0fk|-minrate:v|%dk|-g|48|-hide_banner|-nostats|-row-mt|1|-tune-content|0|-tile-columns|1|-aq-mode|0|-overshoot-pct|0|-undershoot-pct|0|-auto-alt-ref|1;\\" $b $b $(echo "$b * 1.5" | bc -l) $(echo "$b * 3" | bc -l) $b
     b=$(expr $b + 1000)
 done
 
