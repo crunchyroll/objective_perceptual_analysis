16800209b412194cecd7086dc2531735ce4ca76a
Christopher Kennedy <ckennedy@ellation.com>
2020-01-29 19:17:38 +0000

    fix svt-av1 cmd lines
    remove twopass, no rate control yet

--- scripts/gen_cmd.sh
+++ scripts/gen_cmd.sh
@@ -8,7 +8,7 @@ testnum="000"
 if [ "$1" != "" ]; then
     testnum="$1"
 fi
-b=1000
+b=3000
 
 printf '#!/bin/sh'
 printf "\n\n"
@@ -21,14 +21,14 @@ printf ' \\
 printf "%d" $(nproc)
 printf ' -t "\\'
 
-while [ $b -le 8000 ]; do
+while [ $b -le 5000 ]; do
     printf "\n%05dX264H264|ffmpeg|twopass|S|mp4|-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-profile:v|high|-preset|slow|-vcodec|libx264|-bf|0|-refs|4|-b:v|%dk|-maxrate:v|%0.0fk|-bufsize:v|%0.0fk|-minrate:v|%dk|-tune|animation|-x264opts|rc-lookahead=48:keyint=96|-keyint_min|48|-g|96|-force_key_frames|expr:eq(mod(n,48),0)|-hide_banner|-nostats;\\" $b $b $(echo "$b * 1.5" | bc -l) $(echo "$b * 3" | bc -l) $b
-    printf "\n%05dAOMAV1|ffmpeg|twopass|S|mp4|-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-vcodec|libaom-av1|-cpu-used|2|-strict|experimental|-b:v|%dk|-maxrate:v|%0.0fk|-bufsize:v|%0.0fk|-minrate:v|%dk|-keyint_min|48|-g|48|-hide_banner|-nostats|-row-mt|1;\\" $b $b $(echo "$b * 1.5" | bc -l) $(echo "$b * 3" | bc -l) $b
-    printf "\n%05dRAV1EAV1|ffmpeg|twopass|S|mp4|-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-vcodec|librav1e|-speed|6|-b:v|%dk|-keyint_min|48|-g|48|-hide_banner|-nostats;\\" $b $b
+    printf "\n%05dAOMAV1|ffmpeg|twopass|S|mp4|-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-vcodec|libaom-av1|-cpu-used|1|-strict|experimental|-b:v|%dk|-maxrate:v|%0.0fk|-bufsize:v|%0.0fk|-minrate:v|%dk|-keyint_min|48|-g|48|-hide_banner|-nostats|-row-mt|1;\\" $b $b $(echo "$b * 1.5" | bc -l) $(echo "$b * 3" | bc -l) $b
+    printf "\n%05dRAV1EAV1|ffmpeg|twopass|S|mp4|-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-vcodec|librav1e|-speed|1|-b:v|%dk|-keyint_min|48|-g|48|-hide_banner|-nostats;\\" $b $b
     # ffmpeg patch seems to have bugs
-    #printf "\n%05dSVTAV1|ffmpeg|vbr||webm|-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-vcodec|libsvt_av1|-preset|6|-b:v|%dk|-maxrate:v|%0.0fk|-bufsize:v|%0.0fk|-minrate:v|%dk|-rc|vbr|-sc_detection|0|-keyint_min|48|-g|48|-hide_banner|-nostats;\\" $b $b $(echo "$b * 1.5" | bc -l) $(echo "$b * 3" | bc -l) $b
+    #printf "\n%05dSVTAV1|ffmpeg|vbr||mp4|-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-vcodec|libsvt_av1|-preset|3|-b:v|%dk|-maxrate:v|%0.0fk|-bufsize:v|%0.0fk|-minrate:v|%dk|-rc|vbr|-keyint_min|48|-g|48|-hide_banner|-nostats;\\" $b $b $(echo "$b * 1.5" | bc -l) $(echo "$b * 3" | bc -l) $b
 
-    printf "\n%05dSVTAV1|SvtAv1EncApp|twopass||mp4|-enc-mode|3|-film-grain|0|-adaptive-quantization|0|-rc|2|-tbr|%d|-intra-period|23|-irefresh-type|1;\\" $b $(echo "$b * 1000" | bc -l)
+    printf "\n%05dSVTAV1|SvtAv1EncApp|vbr||mp4|-enc-mode|3|-film-grain|0|-adaptive-quantization|0|-rc|1|-tbr|%d|-intra-period|48|-irefresh-type|1;\\" $b $b
 
     # not enabled by default, MacOS doesn't dig it
     #printf "\n%05dSVTVP9|SvtVp9EncApp|vbr||mp4|-enc-mode|0|-tune|2|-rc|1|-tbr|%d|-vbv-maxrate|%0.0f|-vbv-bufsize|%0.0f|-intra-period|48|-irefresh-type|2;\\" $b $(echo "$b * 1000" | bc -l) $(echo "$b * 1000 * 1.5" | bc -l) $(echo "$b * 1000 * 3" | bc -l)
