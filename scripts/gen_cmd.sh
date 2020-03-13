80f6399827996d26d9881e18b98d46550c19c855
Christopher Kennedy <ckennedy@ellation.com>
2020-03-13 23:41:44 +0000

    use svtav1 in ffmpeg
    looks like this works well now.

--- scripts/gen_cmd.sh
+++ scripts/gen_cmd.sh
@@ -30,9 +30,9 @@ while [ $b -le 8000 ]; do
     printf "\n%05dAOMAV1|ffmpeg|twopass|S|mp4||-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-vcodec|libaom-av1|-cpu-used|1|-strict|experimental|-b:v|%dk|-maxrate:v|%0.0fk|-bufsize:v|%0.0fk|-minrate:v|%dk|-keyint_min|48|-g|48|-hide_banner|-nostats|-row-mt|1;\\" $b $b $(echo "$b * 1.5" | bc -l) $(echo "$b * 3" | bc -l) $b
     printf "\n%05dRAV1EAV1|ffmpeg|twopass|S|mp4||-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-vcodec|librav1e|-speed|3|-b:v|%dk|-keyint_min|48|-g|48|-hide_banner|-nostats;\\" $b $b
     # ffmpeg patch seems to have bugs
-    #printf "\n%05dSVTAV1|ffmpeg|vbr||mp4||-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-vcodec|libsvt_av1|-preset|3|-b:v|%dk|-maxrate:v|%0.0fk|-bufsize:v|%0.0fk|-minrate:v|%dk|-rc|vbr|-keyint_min|48|-g|48|-hide_banner|-nostats;\\" $b $b $(echo "$b * 1.5" | bc -l) $(echo "$b * 3" | bc -l) $b
+    printf "\n%05dSVTAV1|ffmpeg|vbr||mp4||-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-vcodec|libsvt_av1|-preset|3|-b:v|%dk|-maxrate:v|%0.0fk|-bufsize:v|%0.0fk|-minrate:v|%dk|-rc|vbr|-keyint_min|48|-g|48|-hide_banner|-nostats;\\" $b $b $(echo "$b * 1.5" | bc -l) $(echo "$b * 3" | bc -l) $b
 
-    printf "\n%05dSVTAV1|SvtAv1EncApp|vbr||mp4||-enc-mode|4|-film-grain|0|-adaptive-quantization|0|-rc|1|-tbr|%d|-intra-period|48|-irefresh-type|1;\\" $b $b
+    #printf "\n%05dSVTAV1|SvtAv1EncApp|vbr||mp4||-enc-mode|4|-film-grain|0|-adaptive-quantization|0|-rc|1|-tbr|%d|-intra-period|48|-irefresh-type|1;\\" $b $b
 
     # not enabled by default, MacOS doesn't dig it
     #printf "\n%05dSVTVP9|SvtVp9EncApp|vbr||mp4||-enc-mode|0|-tune|2|-rc|1|-tbr|%d|-vbv-maxrate|%0.0f|-vbv-bufsize|%0.0f|-intra-period|48|-irefresh-type|2;\\" $b $(echo "$b * 1000" | bc -l) $(echo "$b * 1000 * 1.5" | bc -l) $(echo "$b * 1000 * 3" | bc -l)
