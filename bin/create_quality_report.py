04d6452b724e04a3ba503d269734e95b3ac4497b
Christopher Kennedy <ckennedy@ellation.com>
2020-08-04 06:08:29 +0000

    fix table format

--- bin/create_quality_report.py
+++ bin/create_quality_report.py
@@ -19,12 +19,14 @@ ap = argparse.ArgumentParser()
 ap.add_argument('-n', '--directory', dest='directory', required=True, help="Name of the tests base directory")
 ap.add_argument('-v', '--vivict_urlbase', dest='vivict_urlbase', default=VIVICT_URLBASE, required=False, help="Web server running vivict player")
 ap.add_argument('-s', '--storage_urlbase', dest='storage_urlbase', default=STORAGE_URLBASE, required=False, help="Web server with videos accessible")
+ap.add_argument('-r', '--reference_label', dest='reference_label', default=REFERENCE_LABEL, required=False, help="Reference label to use for comparisons")
 ap.add_argument('-d', '--debug', dest='debug', required=False, action='store_true', help="Debug")
 args = vars(ap.parse_args())
 
 base_directory = args['directory']
 vivict_urlbase = args['vivict_urlbase']
 storage_urlbase = args['storage_urlbase']
+reference_label = args['reference_label']
 debug = args['debug']
 
 encode_dir = "%s/encodes" % base_directory
@@ -62,21 +64,21 @@ for r in results.splitlines():
                         # skip .dotfiles
                         continue
                 #print "encode: %s label: %s" % (encode, label)
-                if REFERENCE_LABEL in e and encode[0:len(encode)-(len(label))] in e:
+                if REFERENCE_LABEL in e and encode[0:len(encode)-(len(label))] in e and e[len(e)-4:] == ".mp4":
                     reference_encode = e
                     encode_list[encode]["reference"] = e
                     encode_list[encode]["encode_file"] = encode_file
 
     elif "Stats: " in r:
-        stats = r
+        stats = r.replace("    Stats: ", "")
         if debug:
-            print "Stats: %s" % stats
-        encode_list[encode]["stats"] = stats.replace("    Stats: ", "");
+            print "Stats: %s" % (stats)
+        encode_list[encode]["stats"] = stats
     elif "Metrics: " in r:
-        metrics = r
+        metrics = r.replace("   Metrics: ", "")
         if debug:
-            print "Metrics: %s" % metrics
-        encode_list[encode]["metrics"] = metrics.replace("   Metrics: ", "")
+            print "Metrics: %s" % (metrics)
+        encode_list[encode]["metrics"] = metrics
     else:
         if debug:
             print "Scene: %s" % r
@@ -87,24 +89,35 @@ for r in results.splitlines():
 if debug:
     print "Encode List: %s" % json.dumps(encode_list, indent=4)
 
-for encode, data in sorted(encode_list.iteritems()):
-    print "Encode: %s" % encode
-    print "Mezzanine: %s" % data["reference"]
-    print "Stats: %s" % data["stats"]
-    print "Metrics: %s" % data["metrics"]
 
+print "<html><title>Encode Quality Comparison %s</title><body><table border=1><caption><h1>Encode Quality Comparsion (%s)</h1></caption>" % (base_directory, base_directory)
+print "<tr><th>Encode</th><th>scenes</th></tr>"
+for encode, data in sorted(encode_list.iteritems()):
+    if debug:
+        print "Encode: %s" % encode
+        print "Mezzanine: %s" % data["reference"]
+        print "Stats: %s" % data["stats"]
+        print "Metrics: %s" % data["metrics"]
+
+    print "<tr>"
+    print "<td><strong>Encode: %s</strong><hr><strong>Reference: (%s) %s</strong><hr><strong>Stats: %s</strong><hr><strong>Metrics: %s</strong></td><td><ol>" % (encode,
+                                                                                                                                           reference_label, data["reference"], data["stats"], data["metrics"])
     for scene in data["scenes"]:
         start_frame, end_frame = scene.split(" ")[0].split("-")
         framerate = 29.976
         position = int(float(start_frame) / framerate)
         duration = int(float(int(end_frame) - int(start_frame)) / framerate)
         # %s?leftVideoUrl=%s&rightVideoUrl=%s&hideSourceSelector=1&hideHelp=1&position=10&duration=10
-        print "Scene: %s" % scene
+        if debug:
+            print "Scene: %s" % scene
         url = "%s?leftVideoUrl=%s&rightVideoUrl=%s&hideSourceSelector=1&hideHelp=1&position=%d&duration=%d" % (vivict_urlbase,
                                                                                                            "%s/%s/%s" % (storage_urlbase, "%s/encodes" % base_directory, "%s.mp4" % encode),
                                                                                                            "%s/%s/%s" % (storage_urlbase, "%s/encodes" % base_directory, data["reference"]),
                                                                                                            position, duration)
-        print "Url: %s" % url
+        if debug:
+            print "Url: %s" % url
+        print "<li><strong>%d-%d)</strong> <a href=%s>%s</a></li>" % (position, position+duration, url, scene)
+    print "</ol></td></tr>"
 
-    print "\n"
+print "</table></html>\n"
 
