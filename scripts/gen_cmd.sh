c8393c5aba0bd20681ba357ac876c45e92f46249
Christopher Kennedy <ckennedy@ellation.com>
2020-08-09 19:24:06 +0000

    update encode test generator
    setup tests with multi resolution and a reference h264 highbitrate

--- scripts/gen_cmd.sh
+++ scripts/gen_cmd.sh
@@ -21,28 +21,36 @@ printf ' \\
 printf "%d" $(nproc)
 printf ' -t "\\'
 
-for resolution in 1080 720 480; do
+# reference encoding
+printf "\n1080p08000kX264H264|ffmpeg|twopass|S|mp4||1080|-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-profile:v|high|-preset|slow|-vcodec|libx264|-bf|0|-refs|4|-b:v|8000k|-maxrate:v|12000k|-bufsize:v|24000k|-minrate:v|8000k|-tune|animation|-x264opts|rc-lookahead=48:keyint=96|-keyint_min|48|-g|96|-force_key_frames|expr:eq(mod(n,48),0)|-hide_banner|-nostats;\\"
+
+for resolution in 2160 1080 720 480 360 240; do
     b=1000
-    max=6000
+    max=7000
     inc=1000
-    totalmax=8000
-    if [ $resolution == 720 ]; then
+    if [ $resolution == 2160 ]; then
+        b=4000
+        max=10000
+        inc=1000
+    elif [ $resolution == 720 ]; then
         b=500
-        max=2000
-        totalmax=4000
+        max=3500
         inc=250
     elif [ $resolution == 480 ]; then
         b=250
-        max=1000
-        totalmax=1500
+        max=2500
         inc=250
+    elif [ $resolution == 360 ]; then
+        b=200
+        max=1800
+        inc=200
+    elif [ $resolution == 240 ]; then
+        b=125
+        max=1000
+        inc=125
     fi
-    while [ $b -le $totalmax ]; do
+    while [ $b -le $max ]; do
         printf "\n%04dp%05dkX264H264|ffmpeg|twopass|S|mp4||%d|-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-profile:v|high|-preset|slow|-vcodec|libx264|-bf|0|-refs|4|-b:v|%dk|-maxrate:v|%0.0fk|-bufsize:v|%0.0fk|-minrate:v|%dk|-tune|animation|-x264opts|rc-lookahead=48:keyint=96|-keyint_min|48|-g|96|-force_key_frames|expr:eq(mod(n,48),0)|-hide_banner|-nostats;\\" $resolution $b $resolution $b $(echo "$b * 1.5" | bc -l) $(echo "$b * 3" | bc -l) $b
-        if [ $b -ge $max ]; then
-            b=$(expr $b + $inc)
-            continue
-        fi
         printf "\n%04dp%05dkAOMAV1|ffmpeg|twopass|S|mp4||%d|-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-vcodec|libaom-av1|-cpu-used|4|-strict|experimental|-b:v|%dk|-maxrate:v|%0.0fk|-bufsize:v|%0.0fk|-minrate:v|%dk|-keyint_min|48|-g|48|-hide_banner|-nostats|-row-mt|1;\\" $resolution $b $resolution $b $(echo "$b * 1.5" | bc -l) $(echo "$b * 3" | bc -l) $b
         printf "\n%05dRAV1EAV1|ffmpeg|twopass|S|mp4||%d|-pix_fmt|yuv420p|-f|mp4|-movflags|+faststart|-vcodec|librav1e|-speed|6|-b:v|%dk|-keyint_min|48|-g|48|-hide_banner|-nostats;\\" $resolution $b $resolution $b
         # ffmpeg patch seems to have bugs
